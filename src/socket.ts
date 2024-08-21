52a69a9aef111b2099307e02010016f94f5fab40
Fedor Kurbatov <kurbatov@gmail.com>
2024-08-21 16:37:42 +0300

    sockets

--- src/socket.ts
+++ src/socket.ts
@@ -2,13 +2,13 @@
 
 import { io, Socket } from "socket.io-client";
 
-// export const socket = io("http://localhost:3000");
+//export const socket = io("http://localhost:3000");
 
 let socket: Socket | null = null;
 
 export const getSocket = (): Socket => {
   if (!socket) {
-    socket = io("http://localhost:3000");
+    socket = io("http://retro.gdao.one");
   }
   return socket;
 };
