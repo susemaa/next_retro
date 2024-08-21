d8fb178c9b8edc6ecf01235dff70f875c4872bd1
Fedor Kurbatov <kurbatov@gmail.com>
2024-08-21 16:40:31 +0300

    typo fix

--- src/socket.ts
+++ src/socket.ts
@@ -2,7 +2,7 @@
 
 import { io, Socket } from "socket.io-client";
 
-//export const socket = io("http://localhost:3000");
+// export const socket = io("http://localhost:3000");
 
 let socket: Socket | null = null;
 
